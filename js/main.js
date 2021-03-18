e9819202b375d494f11db8c6d5a6b22c02595098
Justin Kousemaker <302635165@student.rocmondriaan.nl>
2021-03-18 14:32:22 +0100

    Dark mode

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
 const button = document.querySelector('.theme-switcher')
-
 button.addEventListener('click', function(){
-    const elements = document.querySelectorAll('body, .card-body, .jumbotron')
+    const elements = document.querySelectorAll('body, .card-body, .list-group-item, .head, .jumbotron, .list-group-item, .white')
     for (let k = 0; k < elements.length; k++){
         elements[k].classList.toggle("themer")
     }
-}
+}   
 ) 
