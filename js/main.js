bebe459bf655091ab4094a61e32d6d31f5d7000b
Justin Kousemaker <302635165@student.rocmondriaan.nl>
2021-03-23 17:44:45 +0100

    Dark Mode Local Storage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-
-const button = document.querySelector('.theme-switcher')
-button.addEventListener('click', function(){
+let darkMode = false;
+const themeButton = document.querySelector('.theme-switcher')
+const swapTheme = function(){
     const elements = document.querySelectorAll('body, .card-body, .list-group-item, .head, .jumbotron, .list-group-item, .white')
     for (let k = 0; k < elements.length; k++){
         elements[k].classList.toggle("themer")
     }
-}   
-) 
+    
+    if (darkMode){
+        darkMode = false;
+        localStorage.setItem("darkMode", darkMode);
+    }
+    else{
+        darkMode = true;
+        localStorage.setItem("darkMode", darkMode);
+    }
+    console.log(darkMode)
+}
+themeButton.addEventListener('click', swapTheme)
+
+const localTheme = localStorage.getItem("darkMode")
+
+if (localTheme == "true"){
+    swapTheme();
+}
+
+const emailButton = document.querySelector('.email-getter')
+emailButton.addEventListener('click', function(){
+    navigator.clipboard.writeText("j.l.kousemaker@gmail.com").then(function(){
+        console.log("succes")
+    }).catch(function(){
+        console.log("failed to copy to clipboard")
+    })
+    alert(copyText.value)
+}
+)
