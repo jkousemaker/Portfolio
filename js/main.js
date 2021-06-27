b0508a6216f54feaabbc61e78305659fb5d2a14f
Justin Kousemaker <j.l.kousemaker@Gmail.com>
2021-06-27 21:46:27 +0200

    Cleanup of jumbotron

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector('.theme-switcher')
 let darkMode = false;
 
 const swapTheme = function(){
-    const elements = document.querySelectorAll('.input, .main, .list-items, .sun-moon, .card1, .card2, .card3, .carddd, .cardd-body, .btn-switch.themer, .points, .question, .points, .card1, .buttons-active, .card:hover, .active-navbut, .purple, .display-3, .list-group, .white, .card, body, .card-body, .list-group-item, .head, .jumbotron, .list-group-item, .white')
+    const elements = document.querySelectorAll('.gcard1, .gcard2, .gcard3, .input, .main, .list-items, .sun-moon, .card1, .card2, .card3, .carddd, .cardd-body, .btn-switch.themer, .points, .question, .points, .card1, .buttons-active, .card:hover, .active-navbut, .purple, .display-3, .list-group, .white, .card, body, .card-body, .list-group-item, .head, .jumbotron, .list-group-item, .white')
 
     for (let i = 0; i < elements.length; i++){
         elements[i].classList.toggle("themer")
