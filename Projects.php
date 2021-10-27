99779862cc232f4160ea3fdd37d34a308a5daaf7
Justin Kousemaker <j.l.kousemaker@Gmail.com>
2021-10-27 03:19:45 +0200

    PHP Update, Projects page

---
+++ Projects.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'includes/db.php';
+
+include 'head.php';
+include 'jumbo.php';
+?>
+
+<section class="project">
+    <h1 class="Title">Projects</h1>
+    <div class="projects">
+        <?php
+            $sql = "SELECT * FROM projects;";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+
+            if ($resultCheck > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<div class = "card">';
+                        echo '<p>' . $row['Name'] . '</p> <p>' .  $row['Language'] . '</p> <img src="' . $row['Img'] . '"> </img> <p>' . $row['Description'] . '</p> <p>' . $row['Date'] .'</p> </div>';
+                    }
+            } else {
+                echo "No results!";
+            }
+        ?>
+    </div>
+</Section>
+
---
+++ jumbo.php
@@ -0,0 +1,32 @@
+<div class="jumbotron jumbotron-fluid">
+		<div class="biography">
+			<p class="display-name">Justin Kousemaker</p>
+			<div class="bio">
+				<p class="introduction">Loves video games and learning about new stuff.</p>
+				<div class="bio-theme-switcher">
+					<div class="switcher">
+						<img src="assets/themeswitcher.png" class="sun-moon" alt="Sun" />
+						
+						<label class="switch">
+								<input type="checkbox" class="themeswitch">
+								<span class="slider round theme-switcher"></span>
+						</label>	
+					</div>							
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="container">
+				<nav>
+					<div class="navb">
+							<a href="index.html" class="buttons-active">Home</a>
+							<a href="Game.html" class="buttons">Tic-Tac-Toe</a>
+							<a href="Gamble.html" class="buttons">Gamble Game</a>
+							<button= class="buttons email-getter" data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="j.l.kousemaker@gmail.com (Click to copy)">
+								Contact me
+							</button>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
